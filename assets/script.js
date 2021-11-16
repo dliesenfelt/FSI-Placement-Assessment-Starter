730616d10dce950b09cb5453e064319fba4da978
dliesenfelt <liesenfelt.code@gmail.com>
2021-11-15 19:20:20 -0800

    Assesment

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,74 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
+// Assigns name
+let yourName = "Dylan Liesenfelt" 
 
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
-
-// We'll use these variables to track the counts of each cookie type
-let gb = 0      // Gingerbread
-let cc = 0      // Chocolate Chip
-let sugar = 0   // Sugar Sprinkle
-
-// Code to update name display 
+// Updates name displayed 
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Gingerbread + button was clicked!')
+//Assigning varriables to different cookie types
+var gb = 0      // Gingerbread
+var cc = 0      // Chocolate Chip
+var sugar = 0   // Sugar Sprinkle
+var total = gb+cc+sugar   // Total ammount of all the different cookies
 
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+// When Ginger Bread "+" button is clicked increases quantity by +1
+document.getElementById('add-gb').addEventListener('click', function() {
+    gb+=1;
+    document.getElementById('qty-gb').innerHTML = gb
+    addTotal()
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+// When Ginger Bread "-" button is clicked lowers quantity by -1
+document.getElementById('minus-gb').addEventListener('click', function() {
+    if (gb > 0) // Makes sure quantity can not be a negative value
+    {
+        gb-=1;
+        document.getElementById('qty-gb').innerHTML = gb
+        subtractTotal()
+    }
+})
+
+// When Choclate Chips "+" button is clicked increases quantity by +1
+document.getElementById('add-cc').addEventListener('click', function() {
+    cc+=1;
+    document.getElementById('qty-cc').innerHTML = cc
+    addTotal()
+})
+
+// When Choclate Chips "-" button is clicked lowers quantity by -1
+document.getElementById('minus-cc').addEventListener('click', function() {
+    if (cc > 0) // Makes sure quantity can not be a negative value
+    {
+        cc-=1;
+        document.getElementById('qty-cc').innerHTML = cc
+        subtractTotal()
+    }
+})
+
+// When Sugar Cookie "+" button is clicked increases quantity by +1
+document.getElementById('add-sugar').addEventListener('click', function() {
+    sugar+=1;
+    document.getElementById('qty-sugar').innerHTML = sugar
+    addTotal()
+})
+
+// When Sugar Cookies "-" button is clicked lowers quantity by -1
+document.getElementById('minus-sugar').addEventListener('click', function() {
+    if (sugar > 0) // Makes sure quantity can not be a negative value
+    {
+        sugar-=1;
+        document.getElementById('qty-sugar').innerHTML = sugar
+        subtractTotal()
+    }
+})
+
+//Function that updates the total when adding cookies 
+function addTotal() {
+    total++; 
+    document.getElementById('qty-total').innerHTML = total
+}
+
+//Function that updates the total when removing copkies
+function subtractTotal() {
+    total--;
+    document.getElementById('qty-total').innerHTML = total
+}
